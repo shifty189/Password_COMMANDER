aa159277cb7a1f4860690810c77d8c785e4ae3b2
shifty189 <shifty189@gmail.com>
2024-06-28 12:17:39 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,4 +1,30 @@
 import tkinter as tk
+from Class import *
+import os
+import pickle
+
+settingsCFG = PasswordConfig()
+docs = f'{os.path.expanduser('~')}\\Documents\\PasswordCommander'
+firstTime = True
+if os.path.exists(docs):
+    cfgfile = open(f'{docs}\\default.cfg', 'rb')
+    settingsCFG = pickle.load(cfgfile)
+    cfgfile.close()
+    firstTime = False
+    if os.path.exists(f'{docs}\\Templates'):
+        ...
+    else:
+        os.makedirs(f'{docs}\\Templates')
+else:
+    os.makedirs(docs)
+    cfgfile = open(f'{docs}\\default.cfg', 'ab')
+    pickle.dump(settingsCFG, cfgfile)
+    cfgfile.close()
+    firstTime = True
+    if os.path.exists(f'{docs}\\Templates'):
+        ...
+    else:
+        os.makedirs(f'{docs}\\Templates')
 
 Main = tk.Tk()
 wordFrame = tk.Frame(Main)
@@ -6,25 +32,19 @@ wordControlFrame = tk.Frame(Main)
 settingsFrame = tk.Frame(Main)
 
 VERSION = '0.03'
-EDITDATE = '6/27/2024'
+EDITDATE = '6/26/2024'
 numberCheck = tk.IntVar()
 letterCheck = tk.IntVar()
 letterCheck2 = tk.IntVar()
 specialCheck = tk.IntVar()
 spaceCheck = tk.IntVar()
-numberCheck.set(1)
-letterCheck.set(1)
-letterCheck2.set(1)
-specialCheck.set(1)
-spaceCheck.set(0)
-clicked = tk.StringVar()
-clicked.set('15')
 
-options = []
-temp = 50
-while temp > 0:
-    options.append(str(temp))
-    temp -= 1
+numberCheck.set(settingsCFG.number)
+letterCheck.set(settingsCFG.letter)
+letterCheck2.set(settingsCFG.LETTER)
+specialCheck.set(settingsCFG.special)
+spaceCheck.set(settingsCFG.space)
+
 
 wordFrame.grid(row=0, column=0)
 wordControlFrame.grid(row=1, column=0)
@@ -41,39 +61,35 @@ clipButton = tk.Button(wordControlFrame, text='Copy', command=lambda: ...)
 clipButton.grid(row=0, column=1)
 
 # settings frame
-lengthOption = tk.OptionMenu(settingsFrame, clicked, *options)
-lengthOption.grid(row=0, column=1)
-lengthLabel = tk.Label(settingsFrame, text='Length')
-lengthLabel.grid(row=0, column=0)
 numberButton = tk.Checkbutton(settingsFrame, text='Numbers',
                               variable=numberCheck,
                               onvalue=1,
                               offvalue=0)
-numberButton.grid(row=1, column=0)
+numberButton.pack()
 letterButton = tk.Checkbutton(settingsFrame, text='Letters (Lower)',
                               variable=letterCheck,
                               onvalue=1,
                               offvalue=0,
                               padx=3)
-letterButton.grid(row=2, column=0)
+letterButton.pack()
 letterButton2 = tk.Checkbutton(settingsFrame, text='Letters (Upper)',
                                variable=letterCheck2,
                                onvalue=1,
                                offvalue=0,
                                padx=3)
-letterButton2.grid(row=3, column=0)
+letterButton2.pack()
 specialButton = tk.Checkbutton(settingsFrame, text='Special Characters',
                                variable=specialCheck,
                                onvalue=1,
                                offvalue=2,
                                padx=7)
-specialButton.grid(row=4, column=0)
+specialButton.pack()
 spaceButton = tk.Checkbutton(settingsFrame, text='space',
                              variable=spaceCheck,
                              onvalue=1,
                              offvalue=2,
                              padx=3)
-spaceButton.grid(row=5, column=0)
+spaceButton.pack()
 
 with open('words.txt', 'r') as file:
     Words = file.readlines()
