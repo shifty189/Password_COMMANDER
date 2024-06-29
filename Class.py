c054893c77f8c9461a85765f1a4bbab12a03f33b
shifty189 <shifty189@gmail.com>
2024-06-28 20:38:48 -0400

    update

--- Class.py
+++ Class.py
@@ -1,13 +1,14 @@
 VERSION = '0.04'
 EDITDATE = '6/28/2024'
 
+
 class PasswordConfig:
-    def __init__(self, length=15, num=True, let=True, LET=True, spec=True, space=True, easy=False):
+    def __init__(self, length=15, num=True, let=True, upper=True, spec=True, space=True, easy=False):
         self.length = length
         self.number = num
         self.letter = let
-        self.LETTER = LET
+        self.LETTER = upper
         self.special = spec
         self.space = space
         self.easypassword = easy
-        self.speciallist = ['!','@','#','$','%','^','&','*','+']
+        self.speciallist = ['!', '@', '#', '$', '%', '^', '&', '*', '+']
